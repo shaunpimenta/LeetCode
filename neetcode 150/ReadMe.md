772ca302d700214fe8c26380e7febc055bf46e0e
shaun <=>
2025-08-27 08:49:35 -0700

    Inital Problems Neet code 150

---
+++ neetcode 150/ReadMe.md
@@ -0,0 +1,8 @@
+### Shaun Pimenta's Neetcode 150 journey
+---
+
+## 1 / 150
+
+| No. | Problem           |
+|:---:|:-----------------:|| 1 | duplicate-integer |
+| 1 | is-anagram |
--- neetcode 150/newproblem.py
+++ neetcode 150/newproblem.py
@@ -2,18 +2,19 @@ import pyperclip
 
 def main():
     problem_name = get_problem_name()
-    file_path = f"neetcode 150/{problem_name}.py"
+    # file_path = f"neetcode 150/{problem_name}.py"
+    file_path = f"{problem_name}.py"
     with open(file_path, "w") as file:
         file.write(f"")
     print(f"Created file: {file_path}")
 
-    with open("neetcode 150/ReadMe.md", "r") as readme:
+    with open("ReadMe.md", "r") as readme:
         lines = readme.readlines()
 
     lines[3] = f"## {len(lines) - 6} / 150\n"
     lines.append(f"| {len(lines) - 6} | {problem_name} |\n")
 
-    with open("neetcode 150/ReadMe.md", "w") as readme:
+    with open("ReadMe.md", "w") as readme:
         readme.writelines(lines)
 
     print(f"Updated ReadMe.md")
