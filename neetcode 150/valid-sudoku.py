86e29c9f9adf5665975df160934d444770c20f52
shaun <=>
2025-09-17 11:58:00 -0700

    Optimized sudoku

--- neetcode 150/valid-sudoku.py
+++ neetcode 150/valid-sudoku.py
@@ -15,15 +15,6 @@ class Solution:
                 else:
                     rows.add(i)
 
-        # for col in board:
-        #     cols=set()
-        #     for i in col:
-        #         if i in cols:
-        #             return False
-        #         elif i==".":
-        #             pass
-        #         else:
-        #             cols.add(i)
         for i in range(9):
             tt=[]
             for t in range(9):
@@ -35,9 +26,6 @@ class Solution:
         for i in range(0,9,3):
             for t in range(1,4):
                 t=board[i][(t-1)*3:t*3]+board[i+1][(t-1)*3:t*3]+board[i+2][(t-1)*3:t*3]
-                # print(board[i+2:t*3],t,i)
-                # t+=board[i+2:t*3]
-                # t+=board[i+1:t*3]
                 print(t)
                 t=[i for i in t if i!="."]
                 if len(set(t))==len(t):
@@ -45,4 +33,40 @@ class Solution:
                 else:
                     print("Here",t)
                     return False
+        return True
+
+
+        #Little Optimized using Hashsets
+        for row in board:
+            rows={}
+            for i in row:
+                if i in rows:
+                    return False
+                elif i==".":
+                    pass
+                else:
+                    rows[i]=1
+        for i in range(9):
+            tt=[]
+            cols={}
+            for t in range(9):
+                if board[t][i] in cols:
+                    return False
+                elif board[t][i]==".":
+                    pass
+                else:
+                    cols[board[t][i]]=1
+        for i in range(0,9,3):
+            for t in range(1,4):
+                tt=board[i][(t-1)*3:t*3]+board[i+1][(t-1)*3:t*3]+board[i+2][(t-1)*3:t*3]
+                squares={}
+                print(t)
+                for j in tt:
+                    if j in squares:
+                        print(squares,j)
+                        return False
+                    elif j==".":
+                        pass
+                    else:
+                        squares[j]=1
         return True
